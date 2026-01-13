ef3dc6145b518b41790618cac3a15378e5b4f13f
Kunal Saini <sainikunal621@gmail.com>
2026-01-13 21:08:57 +0530

    rout

--- proxy.js
+++ proxy.js
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
-    '/home',
+    '/',
     '/sign-in(.*)',
     '/sign-up(.*)',
     '/api/webhook(.*)',
